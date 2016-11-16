9dac2d0333969b7a5134f83acb03112737b5527f
ElineJ <elinejacobse@gmail.com>
2016-11-16 11:39:48 +0100

    Uploaded files for week 3

--- Homework/Week-3/SVG1/main1.js
+++ Homework/Week-3/SVG1/main1.js
@@ -1,10 +1,14 @@
 /* use this to test out your function */
 window.onload = function() {
- 	// changeColor();
+    changeColor("no","#deebf7");
+    changeColor("se","#9ecae1");
+    changeColor("fi","#3182bd");
+    changeColor("it","green");
 }
 
 /* changeColor takes a path ID and a color (hex value)
    and changes that path's fill color */
 function changeColor(id, color) {
-        
+
+    document.getElementById(id).style.fill = color;
 }
